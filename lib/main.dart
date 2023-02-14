55874b81b5242956756d2c525f838b0a7fd4892e
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-02-14 10:09:56 -0300

    adicionado as imagens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:foodui/pages/StarterPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,15 +7,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Food Ui',
       debugShowCheckedModeBanner: false,
-      home: ,
+      home: StartPage(),
     );
   }
 }
-
---
+++ lib/pages/StarterPage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class StartPage extends StatefulWidget {
+  const StartPage({super.key});
+
+  @override
+  State<StartPage> createState() => _StartPageState();
+}
+
+class _StartPageState extends State<StartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Container(
+        color: Colors.yellow,
+      ),
+    );
+  }
+}
