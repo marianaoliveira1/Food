f706a1a2c440e727d20b5fa28ed28c13bb64adb6
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-02-14 11:11:47 -0300

    home

--- lib/pages/StarterPage.dart
+++ lib/pages/StarterPage.dart
@@ -13,9 +13,78 @@ class StartPage extends StatefulWidget {
 class _StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        color: Colors.yellow,
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/starter-image.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.bottomCenter,
+            colors: [
+              Colors.black.withOpacity(.9),
+              Colors.black.withOpacity(.8),
+              Colors.black.withOpacity(.2),
+            ],
+          )),
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                const Text(
+                  'Recebendo pedido para entrega',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50,
+                      fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Veja os restaurantes próximos \nadicionando a localização",
+                  style:
+                      TextStyle(color: Colors.white, height: 1.4, fontSize: 18),
+                ),
+                const SizedBox(
+                  height: 100,
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      gradient: LinearGradient(
+                          colors: [Colors.yellow, Colors.orange])),
+                  child: MaterialButton(
+                    minWidth: double.infinity,
+                    onPressed: () {},
+                    child: Text(
+                      "Entre",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Align(
+                  child: Text(
+                    "Entregue à sua porta 24 horas por dia, 7 dias por semana",
+                    style: TextStyle(color: Colors.white70, fontSize: 15),
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
